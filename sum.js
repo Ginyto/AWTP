b34d79778c079eddf2e5cca3329d6fc205f4e51d
Ginyto <agianlucca@gmail.com>
2022-08-30 13:55:53 +0200

    exo 1 done

--- sum.js
+++ sum.js
@@ -1,5 +1,22 @@
-function sum(a, b) {
-    return a + b;
+function sum() {
+
+    if (arguments.length === 0) {
+        return Error('No argument');
+    }
+
+    else {
+
+        let somme = 0;
+
+        for (let index = 0; index < arguments.length; index++) {
+
+            somme += arguments[index]
+
+        }
+
+        return somme
+    }
+
 }
 
 module.exports = sum;
