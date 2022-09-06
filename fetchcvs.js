94a99af36e6dfadf93fd51ba90cc5435bd393b79
Ginyto <agianlucca@gmail.com>
2022-09-06 12:47:34 +0200

    import csv to understand

--- fetchcvs.js
+++ fetchcvs.js
@@ -1,12 +1,19 @@
 const fetch = require('node-fetch')
+
 // This code downloads a CSV file from my website, reads it as text
 // and calls `processData(csvText)` on success. Do not worry about
 // the details about `fetch` for now, as we will cover them later.
+
 fetch('https://thomas-veillard.fr/wp-content/uploads/2021/07/apache-contributors-projects.csv')
     .then(res => res.text())
     .then(processData)
     .catch(console.log)
 
 function processData(csvText) {
+
+    var data = []
+
+    
+
     console.log(csvText)
 }
