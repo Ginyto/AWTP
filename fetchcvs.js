8274d05c99b568dd0e6e418bf13b31288beea158
Ginyto <agianlucca@gmail.com>
2022-09-06 12:30:05 +0200

    exo 4 start

---
+++ fetchcvs.js
@@ -0,0 +1,11 @@
+// This code downloads a CSV file from my website, reads it as text
+// and calls `processData(csvText)` on success. Do not worry about
+// the details about `fetch` for now, as we will cover them later.
+fetch('https://thomas-veillard.fr/wp-content/uploads/2021/07/apache-contributors-projects.csv')
+    .then(res => res.text())
+    .then(processData)
+    .catch(console.log)
+
+function processData(csvText) {
+    console.log(csvText)
+}
