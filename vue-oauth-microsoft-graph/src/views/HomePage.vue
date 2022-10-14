5c2093240605720b44aaa8039b75e43e8ed7986f
Ginyto <agianlucca@gmail.com>
2022-10-14 11:54:28 +0200

    Ex 4: create HomePage component

---
+++ vue-oauth-microsoft-graph/src/views/HomePage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>This is a HomePage</div>
+</template>
+
+<script>
+export default {
+  name: "Test",
+  created() {},
+  data() {
+    return {};
+  },
+  props: {},
+  methods: {},
+};
+</script>
+
+<style lang="css" scoped></style>
