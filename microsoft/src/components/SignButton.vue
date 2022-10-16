eec2a29bb5acd81be3a480c8bbbab662fbddc973
Ginyto <agianlucca@gmail.com>
2022-10-16 15:48:23 +0200

    maj

--- microsoft/src/components/SignButton.vue
+++ microsoft/src/components/SignButton.vue
@@ -1,50 +1,43 @@
 <template>
-
   <div>
     <h1>Sign In To Microsoft</h1>
 
     <div class="sign-in">
       <div class="sign-in__button">
         <button @click="signIn">Sign In</button>
-        <div v-if="user.name"> Voici votre identité : {{user.name}} et votre address mail : {{user.username}}</div>
+        <div v-if="user.name">
+          Voici votre identité : {{ user.name }} et votre address mail :
+          {{ user.username }}
+        </div>
       </div>
     </div>
   </div>
-
 </template>
 
 <script>
-
-import { signInAndGetUser } from '../microsoftGraph.js'
+import { signInAndGetUser } from "../microsoftGraph.js";
 export default {
-
   created() {},
   data() {
     return {
-      user : {}
+      user: {},
     };
   },
   props: {},
   methods: {
-
     signIn() {
-
       signInAndGetUser()
-        .then(user => {
+        .then((user) => {
           this.user = user;
-          this.$emit('user', user);
+          this.$emit("user", user);
           console.log(user);
         })
-        .catch(error => {
+        .catch((error) => {
           console.log(error);
         });
-
-    }
-  
+    },
   },
 };
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
